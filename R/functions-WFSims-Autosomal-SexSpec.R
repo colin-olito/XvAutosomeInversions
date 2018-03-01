8722579f93a954a3d8f78897b37076e144ffba0b
Colin Olito <colin.olito@gmail.com>
2018-03-01 11:05:08 -0900

    issue #3: Removed X-linked rejection sampler. removed unnecessary comments

--- R/functions-WFSims-Autosomal-SexSpec.R
+++ R/functions-WFSims-Autosomal-SexSpec.R
@@ -37,46 +37,6 @@ rejectionSampler  <-  function(n=100, Ne=100, u=1e-6, h=0, s=0.01) {
 }
 
 
-#' Sample approximate stationary distribution for deleterious
-#' mutations at n X-linked loci  
-#'
-#' @title Rejection Sampler for autosomal loci
-#' @param n  Number of loci to sample.
-#' @param Ne Effective population size.
-#' @param u  Mutation rate (default value of u = 1e-6).
-#' @param h  Dominance of deleterious mutations (default value of h = 0).
-#' @param sf Selection against deleterious mutations in females.
-#' @param sm Selection against deleterious mutations in males.
-#' @export
-#rejectionSamplerX  <-  function(n=100, Ne=100, u=1e-6, h=0, sf=0.01, sm=0.01) {
-#	
-#	# Empty vector for allele frequencies
-#	qi  <-  rep(0,times=n)
-#	
-#	# Rejection sampler loop
-#	for(i in 1:n) {
-#		accept  <-  FALSE
-#		while(accept == FALSE) {
-#			x  <-  rbeta(1, shape1=4*Ne*u, shape2=4*Ne*u)
-#			U  <-  runif(1)
-#			if(U < exp((4/3)*Ne*x*(2*h*sf*(x-1)-x*sf-sm))) {
-#				qi[i]   <-  rbinom(1,Ne,x)/Ne
-#				accept  <-  TRUE
-#			}
-#		}
-#	}
-#	qi
-#}
-
-
-# Whoeveer is going to do the Autosomal model with sex-specific selection
-# should start writing their functions here (using the code provided in 
-# './R/functions-WFSims-Autosomal.R' as a template)...
-#
-# just clone into the github repository, then run 
-#
-
-
 #' Linkage Disequilibrium function for W-F recursions
 #'
 #' @title Linkage Disequilibrium (Dstar)
