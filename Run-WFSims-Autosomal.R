ae21ec33dece049672fc4428f8120c568c51d972
Colin Olito <colin.olito@gmail.com>
2018-03-04 19:17:00 -0900

    minor changes

--- Run-WFSims-Autosomal.R
+++ Run-WFSims-Autosomal.R
@@ -24,16 +24,16 @@ source('R/functions-WFSims-Autosomal.R')
 ##  Run Simulations
 
 # Locally adaptive alleles recessive
-recData  <-  makeReplicateAutoInvSimsData(nReps = 3e+5, N.vals = c(500, 1000), m.vals = c(0.005, 0.01), 
-										  s = 0.05, h = 0, r = 0.1, n = 100, u = 1e-5, h.del = 0, 
-										  newMutant = 'random') 
+recessiveData  <-  makeReplicateAutoInvSimsData(nReps = 3e+5, N.vals = c(500, 1000), m.vals = c(0.005, 0.01), 
+												s = 0.05, h = 0, r = 0.1, n = 100, u = 1e-5, h.del = 0, 
+												newMutant = 'random') 
 
 # Additive fitness effects
-addData  <-  makeReplicateAutoInvSimsData(nReps = 3e+5, N.vals = c(500, 1000), m.vals = c(0.005, 0.01), 
-										  s = 0.05, h = 1/2, r = 0.1, n = 100, u = 1e-5, h.del = 0, 
-										  newMutant = 'random') 
+additiveData  <-  makeReplicateAutoInvSimsData(nReps = 3e+5, N.vals = c(500, 1000), m.vals = c(0.005, 0.01), 
+												s = 0.05, h = 1/2, r = 0.1, n = 100, u = 1e-5, h.del = 0, 
+												newMutant = 'random') 
 
 # Locally adaptive alleles dominant
-domData  <-  makeReplicateAutoInvSimsData(nReps = 3e+5, N.vals = c(500, 1000), m.vals = c(0.005, 0.01), 
-										  s = 0.05, h = 1, r = 0.1, n = 100, u = 1e-5, h.del = 0, 
-										  newMutant = 'random') 
+dominantData  <-  makeReplicateAutoInvSimsData(nReps = 3e+5, N.vals = c(500, 1000), m.vals = c(0.005, 0.01), 
+												s = 0.05, h = 1, r = 0.1, n = 100, u = 1e-5, h.del = 0, 
+												newMutant = 'random') 
