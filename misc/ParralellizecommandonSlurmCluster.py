742274fc8fea2b82a64b4b2991535f97430f077c
ludoUPPMAX <dutoit.ludovic@gmail.com>
2018-04-04 00:32:34 +0200

    adding server files

---
+++ misc/ParralellizecommandonSlurmCluster.py
@@ -0,0 +1,17 @@
+#Parameters
+
+commandfile 
+nreplicatespercommand
+launchjobs =True
+#1
+get all commands
+
+# 2 
+
+
+for command in commands:
+	create a subfolder
+	change number of replicates and output file
+	create a .sh and a .R
+	if launchjobs:
+		sbatch
